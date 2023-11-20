68a928bd66c6faef14c0aaf620095839da86e189
Scott Premo <scpremo@hotmail.com>
2023-11-20 13:10:53 -0800

    my answer to lab3

--- kern/pmap.c
+++ kern/pmap.c
@@ -167,6 +167,7 @@ mem_init(void)
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+	envs = (struct Env*)boot_alloc(NENV * sizeof(struct Env));
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -199,7 +200,7 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
-
+	boot_map_region(kern_pgdir, UENVS, PTSIZE, PADDR((void *)envs), PTE_U | PTE_P);
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
 	// stack.  The kernel stack grows down from virtual address KSTACKTOP.
@@ -560,9 +561,20 @@ int
 user_mem_check(struct Env *env, const void *va, size_t len, int perm)
 {
 	// LAB 3: Your code here.
-
+	uintptr_t adr = (uintptr_t) va ; 
+	while(adr < (uintptr_t) va + len){
+		
+		pte_t *pte = NULL;
+		struct PageInfo *page = page_lookup(env->env_pgdir, (void*)adr, &pte);
+		if(!page ||!(*pte & perm) || adr >=ULIM){
+			user_mem_check_addr = adr;
+			return -E_FAULT;
+			} 
+		adr = ROUNDDOWN(adr+PGSIZE,PGSIZE);
+	}
 	return 0;
 }
+
 
 //
 // Checks that environment 'env' is allowed to access the range
